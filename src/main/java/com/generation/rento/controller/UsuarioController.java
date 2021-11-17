0271e3a55ab2d757137e563b0ed394e8aada7636
beatriz-rodrig <beatriz_rodrigues1993@hotmail.com>
2021-11-17 13:36:54 -0300

    :rocket: Metodo Put em usuario

--- src/main/java/com/generation/rento/controller/UsuarioController.java
+++ src/main/java/com/generation/rento/controller/UsuarioController.java
@@ -8,13 +8,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.rento.model.Usuario;
 import com.generation.rento.model.UsuarioLogin;
+import com.generation.rento.repository.UsuarioRepository;
 import com.generation.rento.service.UsuarioService;
 
 @RestController
@@ -24,17 +27,25 @@ import com.generation.rento.service.UsuarioService;
 public class UsuarioController {
 
 	@Autowired
+	private UsuarioRepository repository;
+	
+	@Autowired
 	private UsuarioService usuarioService;
 
-	@GetMapping("/all")
+	@GetMapping
 	public ResponseEntity<List<Usuario>> GetAll() {
-		return ResponseEntity.ok(usuarioService.listarUsuarios());
+		return ResponseEntity.ok(repository.findAll());
 	}
-
+	
 	@PostMapping("/logar")
 	public ResponseEntity<UsuarioLogin> AutenticationUsuario(@RequestBody Optional<UsuarioLogin> usuario) {
 		return usuarioService.logarUsuario(usuario).map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> GetById(@PathVariable long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
 
 	@PostMapping("/cadastrar")
@@ -44,5 +55,11 @@ public class UsuarioController {
 				.map(resp -> ResponseEntity.status(HttpStatus.CREATED).body(resp))
 				.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
 	}
+	
+	@PutMapping
+	public ResponseEntity<Usuario> Put(@RequestBody Usuario usuario) {
+		return ResponseEntity.ok(repository.save(usuario));
+	}
+	
 
 }
